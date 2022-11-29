fad04596f95e23dff1cfcc2fc3b0fc9209c219dd
Thomas George <tqgeorgework@gmail.com>
2022-11-29 10:48:31 -0500

    Initial commit

---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app" class="main">
+    <h1>{{ product.name }}</h1>
+    <p class="description">{{ product.description }}</p>
+    <div class="actions">
+      <router-link :to="{name: 'products'}">Back to Products</router-link>&nbsp;|
+      <router-link :to="{name: 'new-review', params: {id: $route.params.id}}">Add Review</router-link>
+    </div>
+    <div class="well-display">
+      <average-summary />
+      <star-summary rating="1" />
+      <star-summary rating="2" />
+      <star-summary rating="3" />
+      <star-summary rating="4" />
+      <star-summary rating="5" />
+    </div>
+    <review-list />
+  </div>
+</template>
+
+<script>
+import AverageSummary from '../components/AverageSummary.vue';
+import ReviewList from '../components/ReviewList.vue';
+import StarSummary from '../components/StarSummary.vue';
+export default {
+  components: { AverageSummary, StarSummary, ReviewList },
+    created() {
+        //console.table(this.$route);
+        const id = this.$route.params.id;
+        this.$store.commit('SET_ACTIVE_PRODUCT', id);
+    },
+    computed: {
+        product() {
+            return this.$store.getters.product;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
